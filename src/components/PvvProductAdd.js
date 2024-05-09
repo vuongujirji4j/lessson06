403ac9088071a3362d58e648cb316cbf4521c139
vuongujirji4j <151615636+vuongujirji4j@users.noreply.github.com>
2024-05-09 11:16:18 +0700

    Update PvvProductAdd.js

--- src/components/PvvProductAdd.js
+++ src/components/PvvProductAdd.js
@@ -1,78 +1,79 @@
 import React, { Component } from 'react'
 
 export default class PvvProductAdd extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            id:0,
-            title:'',
-            status:0
+        this.state = {
+            id: 0,
+            title: '',
+            status: 0
         }
 
     }
-    pvvHandleChange =(event)=>{
-        let name =event.target.name;
-        let value =event.target.value;
+    pvvHandleChange = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
         this.setState({
-            [name]:value
+            [name]: value
         })
     }
     //submid form
-    pvvHandleSubmit=(ev)=>{
-        ev.preventDafault();
+    pvvHandleSubmit = (ev) => {
+        console.log("Form2", this.state);
+        ev.preventDefault();
         this.props.onSummit(this.state);
     }
-  render() {
-    return (
-      <div>
-        <h2>them moi san pham</h2>
-        <form className='col-md-6'>
-           
-            <div className="input-group mb-3">
-  <span className="input-group-text" id="basic-addon1">
-    Id
-  </span>
-  <input
-    type="text"
-    className="form-control"
-   
-    name='id'
-    value={this.state.id}
-    onChange={this.pvvHandleChange}
-  />
-</div>
-<div className="input-group mb-3">
-  <span className="input-group-text" id="title">
-    Title
-  </span>
-  <input
-    type="text"
-    className="form-control"
-   
-    name='title'
-    value={this.state.title}
-    onChange={this.pvvHandleChange}
+    render() {
+        return (
+            <div>
+                <h2>them moi san pham</h2>
+                <form className='col-md-6'>
 
-  />
-</div>
-<div className="input-group mb-3">
-  <span className="input-group-text" id="title">
-    State
-  </span>
-  <input
-    type="text"
-    className="form-control"
-   
-    name='state'
-    value={this.state.title}
-    onChange={this.pvvHandleChange}
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">
+                            Id
+                        </span>
+                        <input
+                            type="text"
+                            className="form-control"
 
-  />
-</div>
-<button className='btn btn-success'onClick={this.pvvHandleSubmit}>ghi lai</button>
+                            name='id'
+                            value={this.state.id}
+                            onChange={this.pvvHandleChange}
+                        />
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="t">
+                            Title
+                        </span>
+                        <input
+                            type="text"
+                            className="form-control"
 
-        </form>
-      </div>
-    )
-  }
+                            name='title'
+                            value={this.state.title}
+                            onChange={this.pvvHandleChange}
+
+                        />
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="title">
+                            State
+                        </span>
+                        <input
+                            type="text"
+                            className="form-control"
+
+                            name='state'
+                            value={this.state.status}
+                            onChange={this.pvvHandleChange}
+
+                        />
+                    </div>
+                    <button className='btn btn-success' onClick={this.pvvHandleSubmit}>ghi lai</button>
+
+                </form>
+            </div>
+        )
+    }
 }
